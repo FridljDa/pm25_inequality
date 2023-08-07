1e7dd27754b22f4908fb3acd22bbea72afa7f6a2
Daniel Fridljand <daniel.fridljand@gmail.com>
2023-08-07 15:01:56 -0700

    add join function based on age

--- R/helpers.R
+++ R/helpers.R
@@ -248,6 +248,12 @@ group_summarize_add_column <- function(df, column, new_col_value) {
 #' @importFrom readr col_integer
 #' @importFrom readr col_factor
 read_data <- function(path) {
+  # Read the first row of the CSV file
+  first_row <- readr::read_csv(path, n_max = 1)
+
+  # Get the column names
+  column_names <- colnames(first_row)
+
   col_types_data <- cols(
     Year = col_integer(),
     min_age = col_integer(), max_age = col_integer(),
@@ -373,5 +379,89 @@ search_files <- function(directory, string, ends_with = FALSE) {
   matching_files <- all_files[grepl(string, basename(all_files))]
 
   return(matching_files)
+}
+
+#' Inner join two data frames and filter by age.
+#'
+#' This function performs an inner join on two data frames, filters based on minimum and maximum age and
+#' removes redundant age columns. It allows for the option of specifying the joining type to be a right outer join.
+#'
+#' @param df1 The first data frame to join.
+#' @param df2 The second data frame to join.
+#' @param by A vector of variable names to join by.
+#' @param right_outer A logical value. If TRUE, the function performs a right outer join.
+#' @return A data frame that is the result of joining df1 and df2 and filtered by min_age and max_age.
+#'
+#' @export
+#' @examples
+#' df1 <- data.frame(id = c(1, 2, 3), min_age = c(20, 25, 30), max_age = c(40, 45, 50))
+#' df2 <- data.frame(id = c(1, 2, 4), min_age = c(30, 35, 40), max_age = c(50, 55, 60))
+#' inner_join_age(df1, df2, by = "id", right_outer = TRUE)
+inner_join_age <- function(df1, df2, by, right_outer = TRUE){
+  if (!all(c("min_age", "max_age") %in% names(df1))) stop("df1 does not have both 'min_age' and 'max_age' columns")
+  if (!all(c("min_age", "max_age") %in% names(df2))) stop("df2 does not have both 'min_age' and 'max_age' columns")
+
+  # Perform an inner join on the two data frames.
+  df3 <- inner_join(df1,
+                    df2,
+                    by = by,
+                    multiple = "all")
+
+  # Filter and mutate based on the right_outer argument.
+  if(right_outer){
+    df3 <- df3 %>%
+      filter(min_age.y <= min_age.x & max_age.x <= max_age.y) %>% # Keep rows where min_age from df2 is less than or equal to min_age from df1, and max_age from df1 is less than or equal to max_age from df2
+      mutate(
+        min_age = min_age.y, max_age = max_age.y # Assign min_age from df2 to min_age and max_age from df2 to max_age
+      )
+  } else {
+    df3 <- df3 %>%
+      filter(min_age.x <= min_age.y & max_age.y <= max_age.x) %>% # Keep rows where min_age from df1 is less than or equal to min_age from df2, and max_age from df2 is less than or equal to max_age from df1
+      mutate(
+        min_age = min_age.x, max_age = max_age.x # Assign min_age from df1 to min_age and max_age from df1 to max_age
+      )
+  }
+
+  # Remove redundant columns.
+  df3 <- df3 %>%
+    select(-c(min_age.y, min_age.x, max_age.x, max_age.y)) # Drop unnecessary age columns
+
+  return(df3)
+}
+
+#' Inner join two data frames using right outer join and filter by age.
+#'
+#' This function is a wrapper for the \code{\link{inner_join_age}} function with the \code{right_outer} parameter set to TRUE.
+#'
+#' @param df1 The first data frame to join.
+#' @param df2 The second data frame to join.
+#' @param by A vector of variable names to join by.
+#' @return A data frame that is the result of joining df1 and df2 using a right outer join and filtered by min_age and max_age.
+#'
+#' @export
+#' @examples
+#' df1 <- data.frame(id = c(1, 2, 3), min_age = c(20, 25, 30), max_age = c(40, 45, 50))
+#' df2 <- data.frame(id = c(1, 2, 4), min_age = c(30, 35, 40), max_age = c(50, 55, 60))
+#' inner_join_age_right_outer(df1, df2, by = "id")
+inner_join_age_right_outer <- function(df1, df2, by){
+  inner_join_age(df1, df2, by, right_outer = TRUE)
+}
+
+#' Inner join two data frames using left outer join and filter by age.
+#'
+#' This function is a wrapper for the \code{\link{inner_join_age}} function with the \code{right_outer} parameter set to FALSE.
+#'
+#' @param df1 The first data frame to join.
+#' @param df2 The second data frame to join.
+#' @param by A vector of variable names to join by.
+#' @return A data frame that is the result of joining df1 and df2 using a left outer join and filtered by min_age and max_age.
+#'
+#' @export
+#' @examples
+#' df1 <- data.frame(id = c(1, 2, 3), min_age = c(20, 25, 30), max_age = c(40, 45, 50))
+#' df2 <- data.frame(id = c(1, 2, 4), min_age = c(30, 35, 40), max_age = c(50, 55, 60))
+#' inner_join_age_left_outer(df1, df2, by = "id")
+inner_join_age_left_outer <- function(df1, df2, by){
+  inner_join_age(df1, df2, by, right_outer = FALSE)
 }
 
