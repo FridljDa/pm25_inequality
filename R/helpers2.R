3f3236e339c1d0343a04d51e7b414d1ba6586f1f
Daniel Fridljand <daniel.fridljand@gmail.com>
2023-08-08 12:20:05 -0700

    update helper function

--- R/helpers2.R
+++ R/helpers2.R
@@ -1,52 +1,63 @@
 
 library(dplyr)
 
-#' Check if a Package is Used in an R Script
+#' Check Package Usage in an R Script
 #'
-#' This function checks if functions from a specified package are used in the given R script.
-#' It returns the names of functions from the package that are used in the script, or NULL if none are used.
-#' Additionally, a message indicating the usage status will be displayed.
+#' This function checks which functions from a given set of packages are used in an R script.
+#' It takes the path to the R script and a vector of package names, then returns the packages that are actually used.
 #'
-#' @param path_to_script A string providing the path to the R script.
-#' @param pkg A string indicating the name of the package to check.
+#' @param path_to_script A string representing the path to the R script.
+#' @param pkgs A character vector containing the names of the packages to check.
 #'
-#' @return A character vector with the names of the functions from the package used in the script,
-#' or NULL if none are used.
+#' @return A character vector containing the names of the packages that are used in the script, or NULL if none of the specified packages are used.
 #' @examples
 #' \dontrun{
-#' result <- check_package_usage("path_to_your_script.R", "dplyr")
-#' cat(result)
+#'   path <- "path/to/your/script.R"
+#'   packages <- c("dplyr", "ggplot2", "tidyr")
+#'   result <- check_package_usage(path, packages)
+#'   print(result)
 #' }
 #' @export
-check_package_usage <- function(path_to_script, pkg) {
+check_package_usage <- function(path_to_script, pkgs) {
 
   # Helper function to get functions from a package
   get_package_functions <- function(package_name) {
     ls(name = asNamespace(package_name))
   }
 
-  # Load the package functions without attaching it
-  pkg_funcs <- get_package_functions(pkg)
-
   # Read the R script
   script_content <- readLines(path_to_script)
 
-  # Check for usage
-  used_funcs <- sapply(pkg_funcs, function(func) {
-    pattern <- paste0("\\b", func, "\\b")
-    any(grepl(pattern, script_content))
-  })
+  # Store packages that are used
+  packages_used <- c()
 
-  # Return the functions used or a message indicating no usage
-  if (length(used_funcs) > 0) {
-    used_functions <- names(used_funcs[used_funcs])
-    message("The following functions from the package are used in the script:\n")
-    return(paste(used_functions, collapse = ", "))
+  # Iterate over packages
+  for (pkg in pkgs) {
+    # Load the package functions without attaching it
+    pkg_funcs <- get_package_functions(pkg)
+
+    # Check for usage
+    used_funcs <- sapply(pkg_funcs, function(func) {
+      pattern <- paste0("\\b", func, "\\b")
+      any(grepl(pattern, script_content))
+    })
+
+    # Check if any functions were used
+    if (any(used_funcs)) {
+      packages_used <- c(packages_used, pkg)
+    }
+  }
+
+  # Return the packages used or a message indicating no usage
+  if (length(packages_used) > 0) {
+    message("The following packages are used in the script:")
+    return(packages_used)
   } else {
-    message("No functions from the specified package are used in the script.")
+    message("No functions from the specified packages are used in the script.")
     return(NULL)
   }
 }
+
 
 
 #' Group and Sum Specified Columns in a Data Frame
