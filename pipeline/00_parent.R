dae00456fee00d3d47b5fa99fae4eac494f72ad0
Daniel Fridljand <daniel.fridljand@gmail.com>
2023-08-22 14:28:47 -0700

    code change

--- pipeline/00_parent.R
+++ pipeline/00_parent.R
@@ -87,7 +87,7 @@ no_figure_scripts_list_full <- script_vector[!grepl("figure", script_vector)]
 
 #------ running scripts of data pipeline--------
 #no_figure_scripts_list <- c("pipeline/14_read_tot_nvss.R")
-no_figure_scripts_list <- no_figure_scripts_list_full[c(11, 14, 15, 19)] #3,14,
+no_figure_scripts_list <- no_figure_scripts_list_full[c(15)] #, 20, 22, 24, 25#3,14,
 #years <- c(2000, 2010, 2016:2011, 2009:2001, 1999:1990)
 #years <- 1994
 years <- 2002
