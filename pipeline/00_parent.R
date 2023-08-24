b7971267281f528acaa7d03f0186a904ba4f0ad4
Daniel Fridljand <daniel.fridljand@gmail.com>
2023-08-24 15:07:42 -0700

    sum up

--- pipeline/00_parent.R
+++ pipeline/00_parent.R
@@ -47,7 +47,7 @@ dir.create(dem.dir, recursive = T, showWarnings = F)
 dem.agr.dir <- file.path(data.dir, "06_dem.agr")
 dir.create(dem.agr.dir, recursive = T, showWarnings = F)
 agr_bys <- c("county", "nation","STATEFP") # c("county","Census_Region","Census_division","hhs_region_number","STATEFP","nation") , "county", "nation"
-agr_bys <- c("county", "nation") #, "nation"
+agr_bys <- c("nation") #, "nation"
 
 paf.dir <- file.path(data.dir, "07_gbd_paf")
 dir.create(paf.dir, recursive = T, showWarnings = F)
@@ -93,7 +93,7 @@ no_figure_scripts_list_full <- script_vector[!grepl("figure", script_vector)]
 #------ running scripts of data pipeline--------
 #no_figure_scripts_list <- c("pipeline/14_read_tot_nvss.R")
 #c(15, 20, 22, 24, 25, 26, 27)
-no_figure_scripts_list <- no_figure_scripts_list_full[c(20, 26)] #3,14,#c(15, 20, 22, 24, 25, 26, 27)
+no_figure_scripts_list <- no_figure_scripts_list_full[c(26)] #3,14,#c(15, 20, 22, 24, 25, 26, 27)
 #no_figure_scripts_list <- no_figure_scripts_list_full[c(26)]#no_figure_scripts_list <- no_figure_scripts_list_full
 
 years <- c(2000, 2010, 2016:2011, 2009:2001, 1999:1990)
