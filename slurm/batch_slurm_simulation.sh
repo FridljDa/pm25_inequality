26b1346e03d66e937302fe77c09502d535322837
Daniel Fridljand <daniel.fridljand@gmail.com>
2023-09-08 17:37:53 -0700

    fix batch submission

--- slurm/batch_slurm_simulation.sh
+++ slurm/batch_slurm_simulation.sh
@@ -1,18 +1,24 @@
+
+
 #!/bin/bash
 
-#SBATCH --job-name=flexible.sim.batch
-#SBATCH -A huber                # group to which you belong
-#SBATCH -t 0-0:01                   # runtime limit (D-HH:MM:SS)
-#SBATCH -o simulation/out/high_dim_sim_out_batch-%j.out
-#SBATCH -e simulation/error_out/high_dim_sim_out_batch-%j.err       # STDERR
-#SBATCH --mail-type=All        # notifications for job done & fail
-#SBATCH --mail-user=daniel.fridljand@embl.de # send-to address
+#SBATCH --job-name=00_parent
 
-# Number of times to submit the job
-num_splits=20
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+
+#SBATCH --mem 20G                    # memory pool for all cores
+#SBATCH --partition=normal
+#SBATCH --time=1-00:00:00
+
+#SBATCH -o /share/pi/mkiang/dfridljand_air_pollution/pm25_inequality/slurm/out/pipeline_out-%j.out
+#SBATCH -e /share/pi/mkiang/dfridljand_air_pollution/pm25_inequality/slurm/error_out/pipeline_er-%j.err          # STDERR
+#SBATCH --mail-type=END,FAIL        # notifications for job done & fail
+#SBATCH --mail-user=daniel.fridljand@gmail.com # send-to address
 
 # Loop to submit the job multiple times
-for ((i=1; i<=num_splits; i++))
+for ((i=1990; i<=2016; i++))
 do
-    sbatch simulation_flexible/job_slurm_simulation.sh $i $num_splits
+    sbatch /share/pi/mkiang/dfridljand_air_pollution/pm25_inequality/slurm/slurm_parent.sh $i
 done
