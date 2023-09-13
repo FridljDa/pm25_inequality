9ee6894340fab73f166d6772ed3b0fdfc81b0f6a
Daniel Fridljand <daniel.fridljand@gmail.com>
2023-09-13 11:24:11 -0700

    fix get population

--- R/population_data.R
+++ R/population_data.R
@@ -32,9 +32,13 @@ get_population_data <- function(agr_by, year, pop.summary.dir = "data/12_populat
     filter(Year == year)
 
   if(agr_by != "county"){
-    pop_summary2 <- pop_summary2 %>%
+    pop_summary2 <- pop_summary2 #%>%
       filter(!(Education == 666 & rural_urban_class == 666) |
-               !(Education == 666 & svi_bin == 666)) %>%
+               !(Education == 666 & svi_bin == 666) |
+               !(Education == 666 & svi_bin1 == 666) |
+               !(Education == 666 & svi_bin2 == 666) |
+               !(Education == 666 & svi_bin3 == 666) |
+               !(Education == 666 & svi_bin4 == 666)) %>%
       filter(!(Education != 666 & Race != "All"))#rural_urban_class == 666 &
   }else{
     pop_summary2 <- pop_summary2 %>%
