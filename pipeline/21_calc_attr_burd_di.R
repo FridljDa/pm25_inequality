0c61b2c9737a61b9584fcab6df9a97c46f362d90
Daniel Fridljand <daniel.fridljand@gmail.com>
2023-10-24 14:59:28 -0700

    Reverted pipeline/21_calc_attr_burd_di.R to version from commit 3f18b55cec50a6147b2bb81b1216b8fda4e46eea

--- pipeline/21_calc_attr_burd_di.R
+++ pipeline/21_calc_attr_burd_di.R
@@ -11,7 +11,7 @@ suppressMessages({
   library(dplyr)
   library(magrittr)
   library(data.table)
-  #
+  #library(tidyverse)
   library(tictoc)
 })
 
@@ -25,7 +25,7 @@ args <- commandArgs(trailingOnly = T)
 
 # TODO delete
 if (rlang::is_empty(args)) {
-  year <- 2015
+  year <- 2002
   agr_by <- "county"
   source <- "nvss"
 
@@ -33,6 +33,8 @@ if (rlang::is_empty(args)) {
   censDir <- "data/05_demog"
   dem_agrDir <- "data/06_dem.agr"
   totalBurdenParsed2Dir <- "data/13_total_burden_rate"
+  #totalBurdenParsed2Dir <- "data/13_total_burden_rate"
+  #
   attr_burdenDir <- "data/14_attr_burd"
 } else {
   year <- args[1]
@@ -41,7 +43,6 @@ if (rlang::is_empty(args)) {
   dem_agrDir <- args[9]
   agr_by <- args[10]
   source <- args[14]
-
   totalBurdenParsed2Dir <- args[17]
   attr_burdenDir <- args[18]
 }
@@ -54,42 +55,24 @@ attr_burdenDir <- file.path(attr_burdenDir, agr_by, source)
 dir.create(attr_burdenDir, recursive = T, showWarnings = F)
 attr_burdenDir <- file.path(attr_burdenDir, paste0("attr_burd_di_", toString(year), ".csv"))
 if (file.exists(attr_burdenDir)) {
-  quit() #TODO
+  quit()
 }
 
 
 tic(paste("calculated attr burden with di et al", year, agr_by, source))
 #----read some data-----
-total_burden <- paste0("data/09_total_burden_parsed/county/nvss/total_burden_nvss_", year, ".csv") %>%
+total_burden <- file.path(totalBurdenParsed2Dir, agr_by, source, paste0("total_burden_", year, ".csv")) %>%
   read_data()
-#total_burden <- file.path(totalBurdenParsed2Dir, agr_by, source, paste0("total_burden_", year, ".csv")) %>%
-#  read_data()
 
 if("Deaths" %in% colnames(total_burden)) total_burden <- total_burden %>% rename(value = Deaths)
 
 meta <- read.csv(file.path(censDir, "meta", paste0("cens_meta_", year, ".csv")))
 files <- list.files(file.path(dem_agrDir, agr_by, year))
-pm_summ <- lapply(files, function(file){
-  file_i <- fread(file.path(dem_agrDir, agr_by, year, file))
-
-  if (!"pm_lower" %in% names(file_i)) {
-    file_i <- file_i %>%
-      mutate(pm_lower = pm)
-  }
-
-  if (!"pm_upper" %in% names(file_i)) {
-    file_i <- file_i %>%
-      mutate(pm_upper = pm)
-  }
-  return(file_i)
-} ) %>% rbindlist(use.names=TRUE)
-#browser()
-
+pm_summ <- lapply(files, function(file) fread(file.path(dem_agrDir, agr_by, year, file))) %>% rbindlist()
 pm_summ <- pm_summ %>% left_join(meta, by = "variable")
 pm_summ <- pm_summ %>% filter(min_age >= 25)
 # if(agr_by != "nation") pm_summ <- pm_summ %>% filter(scenario == "real")
 # pm_summ <- pm_summ %>% mutate(min_age = min(min_age), max_age = max(max_age))
-
 
 pm_summ <- pm_summ %>% mutate_at(c("rural_urban_class", "Education"), as.factor)
 total_burden <- total_burden %>% mutate_at(c("rural_urban_class", "Education"), as.factor)
@@ -100,8 +83,7 @@ total_burden <- total_burden %>%
   filter(min_age >= 25)
 
 pm_summ <- pm_summ %>%
-  dplyr::group_by_at(vars(one_of("Year", agr_by, "Race", "Hispanic.Origin", "Gender.Code", "Education",
-                                 "rural_urban_class", "scenario", "pm", "pm_lower", "pm_upper", "min_age", "max_age"))) %>%
+  dplyr::group_by_at(vars(one_of("Year", agr_by, "Race", "Hispanic.Origin", "Gender.Code", "Education", "rural_urban_class", "scenario", "pm", "min_age", "max_age"))) %>%
   dplyr::summarize(pop_size = sum(pop_size))
 
 # rm(meta, files)
@@ -141,80 +123,36 @@ hr <- rbind(hr_race_specific, hr_race_uniform)
 rm(hr_race_specific, hr_race_uniform)
 
 hr$max_age <- 150
-
-anti_join <- diagnose_join_issues(df1 = pm_summ,
-                                  df2 = hr,
-                                  join_cols = c("Race", "Hispanic.Origin"))
-if(nrow(anti_join) > 0){
-  warning("diagnose_join_issues() in 21_calc_attr_burd_di.R: pm_summ, hr")
-}
-
 paf_di <- inner_join_age_right_outer(pm_summ,
   hr,
   by = c("Race", "Hispanic.Origin")
 )
-
-#paf_di <- paf_di %>%
-#  dplyr::group_by_at(vars(one_of(setdiff(colnames(paf_di), c("pm", "pop_size"))))) %>%
-#  summarise(pop_weight_pm_exp = weighted.mean(pm, pop_size)) %>%
-#  ungroup()
-#paf_di <- paf_di[1:200, ]
-# Assuming delta_method_weighted_avg function is already defined
-
-#paf_di <- paf_di %>% #TODO
-#  ungroup() %>%
-#  sample_n(2000)
-
 paf_di <- paf_di %>%
-  dplyr::group_by_at(vars(one_of(setdiff(colnames(paf_di), c("pm", "pm_lower", "pm_upper", "pop_size"))))) %>%
-  do({
-    pm = .$pm
-    pm_lower = .$pm_lower
-    pm_upper = .$pm_upper
-    pop_size = .$pop_size
-    result = delta_method_weighted_avg(pm, pm_lower, pm_upper, pop_size)
-    data.frame(pop_weight_pm_exp = result$point_estimate,
-               pop_weight_pm_exp_lower = result$ci_lower,
-               pop_weight_pm_exp_upper = result$ci_upper)
-  }) %>%
+  dplyr::group_by_at(vars(one_of(setdiff(colnames(paf_di), c("pm", "pop_size"))))) %>%
+  summarise(pop_weight_pm_exp = weighted.mean(pm, pop_size)) %>%
   ungroup()
 
-# colnames(paf_di)
-#[1] "Year"                    "county"                  "Race"                    "Hispanic.Origin"
-#[5] "Gender.Code"             "Education"               "rural_urban_class"       "scenario"
-#[9] "method"                  "label_cause"             "hr_mean"                 "hr_lower"
-#[13] "hr_upper"                "min_age"                 "max_age"                 "pop_weight_pm_exp"
-#[17] "pop_weight_pm_exp_lower" "pop_weight_pm_exp_upper"
+
 rm(pm_summ, hr)
-# Assuming delta_method_product is a function defined somewhere in your code
-#browser()
-
 paf_di <- paf_di %>%
-  rowwise() %>%
-  do({
-    mean_x = .$pop_weight_pm_exp - 5
-    lb_x = .$pop_weight_pm_exp_lower - 5
-    ub_x = .$pop_weight_pm_exp_upper - 5
+  mutate(
+    # PAF = 1-1/HR #TODO
+    paf_mean = case_when(
+      pop_weight_pm_exp < 5 ~ 0,
+      pop_weight_pm_exp >= 5 ~ (pop_weight_pm_exp - 5) * (hr_mean - 1) / 10
+    ),
+    paf_lower = case_when(
+      pop_weight_pm_exp < 5 ~ 0,
+      pop_weight_pm_exp >= 5 ~ (pop_weight_pm_exp - 5) * (hr_lower - 1) / 10
+    ),
+    paf_upper = case_when(
+      pop_weight_pm_exp < 5 ~ 0,
+      pop_weight_pm_exp >= 5 ~ (pop_weight_pm_exp - 5) * (hr_upper - 1) / 10
+    ),
+    pop_weight_pm_exp = NULL, hr_upper = NULL, hr_mean = NULL, hr_lower = NULL
+  )
 
-    mean_y = .$hr_mean - 1
-    lb_y = .$hr_lower - 1
-    ub_y = .$hr_upper - 1
-
-    result = delta_method_product(mean_x, lb_x, ub_x, mean_y, lb_y, ub_y, alpha = 0.05)
-
-    data.frame(., paf_mean = result$mean, paf_lower = result$lb, paf_upper = result$ub)
-  })
-
-paf_di <- paf_di %>%
-   mutate(paf_mean = pmax(0, paf_mean),
-           paf_lower = pmax(0, paf_lower),
-           paf_upper = pmax(0, paf_upper))
-#"hr_mean"                 "hr_lower"                "hr_upper"
-#"pop_weight_pm_exp"       "pop_weight_pm_exp_lower" "pop_weight_pm_exp_upper"
 #if (agr_by == "county") paf_di$rural_urban_class <- as.factor(666) # TODO change total burden
-# Remove specific columns using the subset function
-paf_di <- paf_di %>%
-  subset(select = -c(hr_mean, hr_lower, hr_upper, pop_weight_pm_exp, pop_weight_pm_exp_lower, pop_weight_pm_exp_upper))
 
 if (agr_by == "county") {
   total_burden <- total_burden %>%
@@ -238,37 +176,16 @@ if ("rural_urban_class" %in% names(paf_di)) {
 if ("svi_bin" %in% names(paf_di)) {
   paf_di <- paf_di %>% select(-svi_bin)
 }
-
-if ("svi_bin1" %in% names(paf_di)) {
-  paf_di <- paf_di %>% select(-svi_bin1)
-}
-if ("svi_bin2" %in% names(paf_di)) {
-  paf_di <- paf_di %>% select(-svi_bin2)
-}
-if ("svi_bin3" %in% names(paf_di)) {
-  paf_di <- paf_di %>% select(-svi_bin3)
-}
-if ("svi_bin4" %in% names(paf_di)) {
-  paf_di <- paf_di %>% select(-svi_bin4)
-}
 #paf_di
 ##---join---
 paf_di <- paf_di %>%
   select(-min_age) %>%
   select(-max_age)
 
-anti_join_paf_di <- diagnose_join_issues(df1 = total_burden,
-                                  df2 = paf_di,
-                                  join_cols = c("Year", agr_by, "Race", "Hispanic.Origin", "Gender.Code", "Education", "label_cause"))
-if(nrow(anti_join_paf_di) > 0){
-  warning("diagnose_join_issues() in 21_calc_attr_burd_di.R: total_burden, paf_di")
-}
-
 attr_burden_di <- left_join(
   total_burden,
   paf_di,
-  by = c("Year", agr_by, "Race", "Hispanic.Origin", "Gender.Code", "Education", "label_cause"),
-  multiple = "all"#,
+  by = c("Year", agr_by, "Race", "Hispanic.Origin", "Gender.Code", "Education", "label_cause")#,
   #relationship = "many-to-many" #paf_di %>% filter(method == "di_gee" & scenario == "real")
 )
 
